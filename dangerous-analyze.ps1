b3efeb0ad00c622213e72bcdc98457b0ff6440d2
Nicolas Cousin <contact@nicolas-cousin.com>
2026-03-02 17:28:12 +0000

    Ajout du script dangerous-analyze.ps1 pour analyser les dépendances et calculer les scores des projets à partir du fichier all-edges.csv.

---
+++ dangerous-analyze.ps1
@@ -0,0 +1,25 @@
+$edges = Import-Csv ".\deps-analysis\all-edges.csv"
+
+$incoming = @{}
+$outgoing = @{}
+
+foreach ($e in $edges) {
+  if (-not $incoming.ContainsKey($e.To)) { $incoming[$e.To] = 0 }
+  if (-not $outgoing.ContainsKey($e.From)) { $outgoing[$e.From] = 0 }
+
+  $incoming[$e.To]++
+  $outgoing[$e.From]++
+}
+
+$projects = ($incoming.Keys + $outgoing.Keys) | Sort-Object -Unique
+
+$result = foreach ($p in $projects) {
+  [PSCustomObject]@{
+    Project = $p
+    Incoming = if ($incoming.ContainsKey($p)) { $incoming[$p] } else { 0 }
+    Outgoing = if ($outgoing.ContainsKey($p)) { $outgoing[$p] } else { 0 }
+    Score = ( ($incoming[$p] + $outgoing[$p]) )
+  }
+}
+
+$result | Sort-Object Score -Descending | Select-Object -First 30
